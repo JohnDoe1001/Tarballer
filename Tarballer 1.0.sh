9c1403ec37596d4f0fd524c5cd43a550aa54be2f
JohnDoe1001 <102891530+JohnDoe1001@users.noreply.github.com>
2023-09-27 19:31:00 +0700

    Update Tarballer 1.0.1.sh

--- Tarballer 1.0.sh
+++ Tarballer 1.0.sh
@@ -11,20 +11,21 @@ sudo apt-get check
 tarball () {
     tar=$(whiptail --inputbox "Please specify the path to the .tar.* file." 10 40 --title "Welcome to the Tarballer 0.5!" 3>&1 1>&2 2>&3)
 	while :
+	do
 		if [[ -d $tar ]];
 		then
-                        a=${cd $tar}
-			b=${tar -zxvf $tar || tar -jxvf $tar}
-			c=${./configure}
-			d=${make}
-			e=${sudo make install}
-                        whiptail --gauge "Redirecting to the dỉrectory..." 15 80 $a
-			whiptail --gauge "Untar in progress..." 15 80 $b
-                        whiptail --gauge "Configuring..." 15 80 $c
-			whiptail --gauge "Compiling..." 15 80 $d
-                        whiptail --gauge "Installing..." 15 80 $e
+                        a=$("cd $tar || exit")
+			b=$(tar -zxvf "$tar" || tar -jxvf "$tar")
+			c=$(./configure)
+			d=$(make)
+			e=$(sudo make install)
+                        whiptail --gauge "Redirecting to the dỉrectory..." 15 80 "$a"
+			whiptail --gauge "Untar in progress..." 15 80 "$b"
+                        whiptail --gauge "Configuring..." 15 80 "$c"
+			whiptail --gauge "Compiling..." 15 80 "$d"
+                        whiptail --gauge "Installing..." 15 80 "$e"
 			whiptail --msgbox "Installation completed!" 40 40
-			if [[$? -ne 0]];
+			if [[ $? -ne 0 ]];
 			then
 				echo "No error found! Good for you!" > good
                 whiptail --textbox good 20 25
@@ -37,6 +38,8 @@ tarball () {
             whiptail --textbox dir 30 35
 			clear
 			tarballer
+        	fi
+        done
 }
 
 tarball
