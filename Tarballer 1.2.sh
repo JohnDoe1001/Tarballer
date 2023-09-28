93d014b8f0daa28e54b629ce47fa6011eb79dc84
JohnDoe1001 <102891530+JohnDoe1001@users.noreply.github.com>
2023-09-28 15:49:31 +0700

    Update and rename Tarballer 1.1.sh to Tarballer 1.2.sh

--- Tarballer 1.2.sh
+++ Tarballer 1.2.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# To change the interface, search for "lol" (without the quotes) and change the function "tar_gui" to "tar_cli"
-
 sudo apt-get update
 sudo apt-get install whiptail
 sudo apt-get upgrade
@@ -9,20 +7,26 @@ sudo apt-get check
 
 tar_gui () {
     tar=$(whiptail --inputbox "Please specify the path to the .tar.* file." 10 40 --title "Welcome to the Tarballer 1.1!" 3>&1 1>&2 2>&3)
-	while :
+    echo $tar
+	while x=1:
 	do
+                x=$x+1
 		if [[ -d $tar ]];
 		then
                         a=$("cd $tar || exit")
-			b=$(tar -zxvf "$tar" || tar -jxvf "$tar")
-			c=$(./configure)
-			d=$(make)
-			e=$(sudo make install)
+			b=$(sudo mkdir ./$RANDOM)
+			c=$(tar -C ./$b -zxvf "$tar" || tar -C ./$b -jxvf "$tar" || tar -C ./$b -xzvf "$tar")
+			d=$(./configure)
+			e=$(make)
+			f=$(sudo make install)
+                        g=$(sudo rm -d ./$b)
                         whiptail --gauge "Redirecting to the dỉrectory..." 15 80 "$a"
-			whiptail --gauge "Untar in progress..." 15 80 "$b"
-                        whiptail --gauge "Configuring..." 15 80 "$c"
-			whiptail --gauge "Compiling..." 15 80 "$d"
-                        whiptail --gauge "Installing..." 15 80 "$e"
+			whiptail --gauge "Creating temporary directory..." 15 80 "$b"
+			whiptail --gauge "Decompressing..." 15 80 "$c"
+                        whiptail --gauge "Configuring..." 15 80 "$d"
+			whiptail --gauge "Compiling..." 15 80 "$e"
+                        whiptail --gauge "Installing..." 15 80 "$f"
+			whiptail --gauge "Finishing installation..." 15 80 "$g"
 			whiptail --msgbox "Installation completed!" 40 40
 			if [[ $? -eq 0 ]];
 			then
@@ -39,22 +43,26 @@ tar_gui () {
 			tar_gui
         	fi
         done
+exit
 }
 
 # for debugging purposes
 tar_cli () {
-echo "Welcome to the Tarballer 1.1!"
+echo "Welcome to the Tarballer 1.2!"
 echo "Please specify the path to the .tar.* file."
 read tarcli
-	while :
+	while x=1:
  	do
+                x=$x+1
   		if [[ -d $tarcli ]];
 		then
   			cd $tar || exit
-  			tar -zxvf "$tar" || tar -jxvf "$tar"
+                        tmptar=$(sudo mkdir ./$RANDOM)
+  			tar -C ./$tmptar -zxvf "$tar" || tar -C ./$tmptar -jxvf "$tar" || tar -C ./$tmptar -xzvf "$tar"
   			./configure
   			make
   			sudo make install
+                        sudo rm -d ./tmptar
      			if [[ $? -eq 0 ]];
 			then
 				echo "No error found! Good for you!"
@@ -65,7 +73,21 @@ read tarcli
 			echo Invalid directory, please try again.
    		fi
 	done
+ exit
 }
 
-# lol
-tar_gui
+prompt=$(whiptail --title "Choose an interface to continue." --menu "Choose an interface" 30 30 5 \
+"Normal" "GUI" \
+"Debug mode" "CLI" \
+"Exit" 3>&1 1>&2 2>&3)
+case $prompt in:
+    "Normal") 
+	    tar_gui
+	;;
+	"Debug mode")
+        tar_cli
+    ;;
+	"Exit")
+        exit
+esac
+ 
